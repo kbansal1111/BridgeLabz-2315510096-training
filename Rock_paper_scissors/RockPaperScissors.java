819ba95c245f4d43b10d7adfaad174e40fc07215
kartik bansal <142658657+kbansal1111@users.noreply.github.com>
2025-08-27 11:01:36 +0530

    Add files via upload

--- Rock_paper_scissors/RockPaperScissors.java
+++ Rock_paper_scissors/RockPaperScissors.java
@@ -1,54 +1,76 @@
-package Rock_paper_scissors;
+package Level_2_Strings_Practice_Problems;
+
 import java.util.Scanner;
-import java.util.Random;
-import java.util.HashMap;
+
 public class RockPaperScissors {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);	
-		HashMap<String,Integer> map=new HashMap<>(); 
-		map.put("User Wins", 0);
-        map.put("Computer Wins", 0);
-        map.put("Tie", 0);		
-		while(true) {
-			String[] choice= {"Rock","Paper","Scissors"};			
-			String computerChoice=computerChoice(choice);			
-			String userChoice=userChoice(choice);		
-			System.out.println(winner(computerChoice,userChoice,map));		
-			System.out.println("Do you want to play again-0 or Exit-1");		
-			int again=sc.nextInt();
-			if(again==1) {
-				int totalScore=map.get("Tie")+map.get("User Wins")+map.get("Computer Wins");
-				System.out.println("Final ScoreCard: "+map);
-				System.out.println("Tie%: "+ (map.get("Tie")*100.0)/totalScore);
-				System.out.println("User Wins%: "+ (map.get("User Wins")*100.0)/totalScore);
-				System.out.println("Computer Wins%: "+ (map.get("Computer Wins")*100.0)/totalScore);				
-				break;
-			}
-		}
-	}
-	public static String computerChoice(String[] choice) {
-		Random random=new Random();
-		return choice[random.nextInt(3)];
-	}	
-	public static String userChoice(String[] choice) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter Your Choice: 0-Rock 1-paper 2-Scissors ");
-		int userChoice=sc.nextInt();
-		return choice[userChoice];
-	}	
-	public static String winner(String computerChoice,String userChoice,HashMap<String,Integer> map) {
-		System.out.println("Computer choose: "+computerChoice);
-		System.out.println("User choose: "+userChoice);
-			
-		if(computerChoice.equals("Rock") && userChoice.equals("Paper")||computerChoice.equals("Scissors") && userChoice.equals("Rock")||computerChoice.equals("Paper") && userChoice.equals("Scissors")) {
-			map.put("User Wins", map.get("User Wins") + 1);
-			return "User Wins";
-		}
-		if(computerChoice.equals("Paper") && userChoice.equals("Rock")||computerChoice.equals("Rock") && userChoice.equals("Scissors")||computerChoice.equals("Scissors") && userChoice.equals("Paper")) {
-			map.put("Computer Wins", map.get("Computer Wins") + 1);
-			return "Computer Wins";
-		}	
-		map.put("Tie",map.get("Tie")+1);
-		return "It's a Tie";		
-	}
+
+    static String getComputerChoice() {
+        String[] choices = {"rock", "paper", "scissors"};
+        return choices[(int)(Math.random() * 3)];
+    }
+
+    static String getWinner(String user, String computer) {
+        if (user.equals(computer)) return "draw";
+        if ((user.equals("rock") && computer.equals("scissors")) ||
+            (user.equals("scissors") && computer.equals("paper")) ||
+            (user.equals("paper") && computer.equals("rock"))) {
+            return "user";
+        } else {
+            return "computer";
+        }
+    }
+
+    static String[][] calculateStats(int userWins, int compWins, int games) {
+        String[][] stats = new String[2][3];
+        stats[0][0] = "User";
+        stats[0][1] = String.valueOf(userWins);
+        stats[0][2] = String.format("%.2f", (userWins * 100.0 / games)) + "%";
+
+        stats[1][0] = "Computer";
+        stats[1][1] = String.valueOf(compWins);
+        stats[1][2] = String.format("%.2f", (compWins * 100.0 / games)) + "%";
+
+        return stats;
+    }
+
+    static void displayGameResults(String[][] rounds, String[][] stats) {
+        System.out.printf("%-10s %-12s %-12s %-10s%n", "Round", "User", "Computer", "Winner");
+        for (String[] round : rounds) {
+            System.out.printf("%-10s %-12s %-12s %-10s%n", round[0], round[1], round[2], round[3]);
+        }
+        System.out.println("\nStats:");
+        System.out.printf("%-10s %-10s %-10s%n", "Player", "Wins", "Win %");
+        for (String[] stat : stats) {
+            System.out.printf("%-10s %-10s %-10s%n", stat[0], stat[1], stat[2]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of games: ");
+        int games = sc.nextInt();
+        sc.nextLine();
+
+        int userWins = 0, compWins = 0;
+        String[][] rounds = new String[games][4];
+
+        for (int i = 0; i < games; i++) {
+            System.out.print("Round " + (i + 1) + " - Enter choice (rock/paper/scissors): ");
+            String user = sc.nextLine().toLowerCase();
+            String computer = getComputerChoice();
+            String winner = getWinner(user, computer);
+
+            if (winner.equals("user")) userWins++;
+            else if (winner.equals("computer")) compWins++;
+
+            rounds[i][0] = "Round " + (i + 1);
+            rounds[i][1] = user;
+            rounds[i][2] = computer;
+            rounds[i][3] = winner;
+        }
+
+        String[][] stats = calculateStats(userWins, compWins, games);
+        displayGameResults(rounds, stats);
+    }
 }
+
